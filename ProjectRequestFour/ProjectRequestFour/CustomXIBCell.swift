fec13cad3c842d03ce132c0287271c6facf82456
Aninha <aninhapss2000@gmail.com>
2023-03-19 16:51:46 -0300

    Added CustomXIBCell

---
+++ ProjectRequestFour/ProjectRequestFour/CustomXIBCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomXIBCell.swift
+//  ProjectRequestFour
+//
+//  Created by Ana Paula Silva de Sousa on 19/03/23.
+//
+
+import UIKit
+
+class CustomXIBCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
